b7fa17e4ea39e15bedd307dc11f1ccddd184d9c1
syma sultana <symasultana2002@gmail.com>
2025-07-01 21:45:54 +0600

    added the delete api

--- index.js
+++ index.js
@@ -147,6 +147,14 @@ async function run() {
 });
 
 
+ app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
 
 
 
